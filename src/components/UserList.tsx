fcd80df5495c968549cf88cef2fbc48601e38fab
ezinber <ezinber@yandex.ru>
2022-02-01 00:16:36 +0300

    feat: initialize app

---
+++ src/components/UserList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+
+const UserList: React.FC = () => {
+  const { users, loading, error } = useTypedSelector(state => state.user)
+
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+export default UserList;
